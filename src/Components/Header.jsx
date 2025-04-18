80f792d2a59d048ea61c41fc08d03fd8aaecf192
Abhishek Bagal <abhibagal34@gmail.com>
2025-04-18 16:20:35 +0530

    Added a note

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,6 +3,7 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header'>
+      <p> Note : Working on the project....................</p>
         <h1><span>X</span>News</h1>
         <h4><span>Read</span> the latest <span>news</span> from <span>around</span> the <span>world </span> <span>.</span></h4>
     </div>
